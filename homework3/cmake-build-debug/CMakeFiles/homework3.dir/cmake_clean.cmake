33cc8c643c615dbc8cca4b20acd812637710b57b
aedg1r <edgar.saratovcev@gmail.com>
2021-03-31 16:08:55 +0300

    added project

---
+++ homework3/cmake-build-debug/CMakeFiles/homework3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework3.dir/main.cpp.obj"
+  "homework3.exe"
+  "homework3.exe.manifest"
+  "homework3.pdb"
+  "libhomework3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework3/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "pugixml/src/pugixml.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
